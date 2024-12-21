07aa40701a9d254fc7536cc73b78b46cf17a6a75
tupta9x <ptratu99@gmail.com>
2024-12-21 08:05:41 +0700

    reset

--- assets/js/404.js
+++
@@ -1,5 +0,0 @@
-let fullPath= window.location.href;
-if (fullPath.indexOf("-unblocked.html")> 0) {
-    fullPath= fullPath.replace("-unblocked.html", "-unblockedz.html");
-    window.location= fullPath;
-}
